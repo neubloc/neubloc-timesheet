b96ae84ec75e5face70efd479efda94fd16afb12
Marcin Rim <mrim@neubloc.net>
2012-02-20 18:31:34 +0100

    setup

--- setup.py
+++ setup.py
@@ -7,25 +7,27 @@ from setuptools import setup, find_packages
 def read(filename):
     return open(os.path.join(os.path.dirname(__file__), filename)).read()
 
-NAME='neubloc-timesheet'
+NAME = 'neubloc-timesheet'
+VERSION = read('VERSION').replace("\n",'')
 
 setup(
-    name=NAME,
-    version=read('VERSION').replace("\n",''),
-    description='Control neubloc timesheet from gtk3 app',
-    author='Linden Lab',
-    author_email='mrim@neubloc.net',
-    packages=find_packages(), #['src', 'tests'],
-    include_package_data=True,
-    long_description=read('README'),
-    classifiers=[
+    name = NAME,
+    version = VERSION,
+    description = 'Control neubloc timesheet from gtk3 app',
+    author = 'Linden Lab',
+    author_email = 'mrim@neubloc.net',
+    packages = find_packages(),
+    package_data = {'neubloc_timesheet.src.static': ['*']},
+    include_package_data = True,
+    long_description = read('README'),
+    classifiers = [
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
         "License :: OSI Approved :: BSD License",
     ],
-    keywords='networking internet neubloc',
-    license='BSD',
-    install_requires=[
+    keywords = 'networking internet neubloc',
+    license = 'BSD',
+    install_requires = [
         'setuptools',
         'mechanize',
         #'PyGObject',
