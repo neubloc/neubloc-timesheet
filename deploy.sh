ecd26e68fa0d5d7bb7b24cc9a84ecc85d1cc4ba8
Marcin Rim <mrim@neubloc.net>
2012-03-05 16:21:14 +0100

    deploy + install

--- deploy.sh
+++ deploy.sh
@@ -12,4 +12,4 @@ scp dist/* storage:/nfs/web/pypi
 
 echo
 echo "*** Installing from pypi.rimek.org"
-easy_install-2.7 http://pypi.m.rimek.org/neubloc_timesheet-0.1-py2.7.egg
+sudo easy_install-2.7 http://pypi.m.rimek.org/neubloc_timesheet-0.1-py2.7.egg
