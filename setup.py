bad8ae350347f740e388843e4c87c68355f7e1ba
Marcin Rim <mrim@neubloc.net>
2012-03-07 09:50:40 +0100

    setup fix

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     install_requires = [
         'setuptools',
         'mechanize',
-        'BeautifulSoup'
+        'BeautifulSoup',
         #'PyGObject',
         #'gconf',
         #'pdb',
