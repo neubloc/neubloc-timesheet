f2cc3310f5da706a22a415f6e21ae7ca9466d23e
Marcin Rim <mrim@neubloc.net>
2012-06-05 16:02:07 +0200

    .

--- neubloc_timesheet/src/gui.py
+++ neubloc_timesheet/src/gui.py
@@ -16,7 +16,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/lib')
 from lib.timesheet import *
 from lib.config import *
 
-from decorators import threaded
+from lib.decorators import threaded
 
 DEBUG = bool(os.getenv('DEBUG'))
 COLORS = { 'green': "#33af95ac3c98", 'red': "#ffff587b587b" }
